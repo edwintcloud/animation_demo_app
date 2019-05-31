cfb5be544a78de692840752842cdf75e6c9509bc
Edwin Cloud <ecloud412@gmail.com>
2019-05-31 12:36:03 -0700

    Initial Commit :rocket:

---
+++ README.md
@@ -0,0 +1,5 @@
+# animation_demo_app
+
+A Flutter application demonstrating simple animations.
+
+![alt text](./demo.gif "Demo")
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: Duration(milliseconds: 3000), vsync: this);
+
+    animation = Tween(begin: 0.0, end: 300.0).animate(animationController);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LogoAnimation(
+      animation: animation,
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+}
+
+class LogoAnimation extends AnimatedWidget {
+  LogoAnimation({Key key, Animation animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation animation = listenable;
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        child: Container(
+          height: animation.value,
+          width: animation.value,
+          child: FlutterLogo(),
+        ),
+        builder: (BuildContext context, Widget _widget) {
+          return Transform.rotate(
+            angle: animation.value / 22,
+            child: _widget,
+          );
+        },
+      ),
+    );
+  }
+}
